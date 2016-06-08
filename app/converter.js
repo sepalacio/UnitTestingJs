90f882129c3d260efcdca9a181aa0a9449a39839
sepalacio <sebastianpalacio327@gmail.com>
2016-06-08 10:41:02 -0500

    Testing app functions for temperature conversor

---
+++ app/converter.js
@@ -0,0 +1,7 @@
+module.exports.toFarenheit = function (temperature){
+    return (temperature * 9/5) + 32;
+}
+
+module.exports.toCelsius = function (temperature){
+    return (temperature - 32) * 5/9;
+}
--- test/converter.js
+++ test/converter.js
@@ -1,15 +1,21 @@
 //Module dependencies
 var expect = require('chai').expect;
+var converter = require('..');
 
 describe('Temperature Conversor', function (){
     
-    describe('Celsius to farenheit conversion', function (){
-        
-        it('Converts 100c to farenheit', function (){
-            converter
-            
+    describe('Celsius to Farenheit conversion', function (){
+        it('Converts 100C to Farenheit', function (){
+          var fh = converter.toFarenheit(100);
+            expect(fh).to.equal(212);
         })
-        
+    })
+    
+    describe('Farenheit to Celsius conversion', function (){
+        it('Converts 212F to Celsius', function (){
+          var cl = converter.toCelsius(212);
+            expect(cl).to.equal(100);
+        })
     })
     
 })
