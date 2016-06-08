1c3b7472d592f6f44ffd33c20bf13a4a9c9ea18f
sepalacio <sebastianpalacio327@gmail.com>
2016-06-08 09:21:08 -0500

    Starting proyect

---
+++ test/converter.js
@@ -0,0 +1,15 @@
+//Module dependencies
+var expect = require('chai').expect;
+
+describe('Temperature Conversor', function (){
+    
+    describe('Celsius to farenheit conversion', function (){
+        
+        it('Converts 100c to farenheit', function (){
+            converter
+            
+        })
+        
+    })
+    
+})
